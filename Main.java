85e7502278e1965c2ec61915e7396c826e2e00ae
aydig8139 <aydig8139@wrdsb.ca>
2020-10-30 15:37:57 +0000

    finished guessing game question

--- Main.java
+++ Main.java
@@ -1,11 +1,26 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Random;
 
 public class Main implements Runnable, ActionListener{
 
   // Class Variables  
+  JPanel mainPanel;
   
+  JLabel instructions;
+  JLabel result;
+
+  JTextField input;
+
+  JButton submitButton;
+  JButton newButton;
+
+  //create a random number generator
+  Random rand = new Random();
+
+  //get the random number generator to get a number
+  int randInt = rand.nextInt(100) + 1;
 
 
   // Method to assemble our GUI
@@ -18,7 +33,56 @@ public class Main implements Runnable, ActionListener{
     frame.setSize(800,600);
     // shows the window
     frame.setVisible(true);
- 
+
+    //initialize the main JPanel
+    mainPanel = new JPanel();
+
+    //disable any layout helpers
+    mainPanel.setLayout(null);
+
+    //create the labels
+    instructions = new JLabel("Guess the number between 0 and 100:");
+    result = new JLabel();
+
+    //layout the labels by setting the coordinate and size 
+    instructions.setBounds(20,20,300,20);
+    result.setBounds(20,110,300,20);
+
+    //initialize the input text field
+    input = new JTextField();
+
+    //set the size and location of the textfield
+    input.setBounds(20, 50, 280, 20);
+
+    //initialize the buttons
+    submitButton = new JButton("Submit");
+    newButton = new JButton("New Number");
+
+    //set the size and location of the buttons
+    submitButton.setBounds(20, 90, 100, 20);
+    newButton.setBounds(150, 90, 150, 20);
+
+    //add an action listener to the buttons
+    submitButton.addActionListener(this);
+    newButton.addActionListener(this);
+
+    //set the action command on the buttons
+    submitButton.setActionCommand("submit");
+    newButton.setActionCommand("new");
+
+    //add the buttons to the panel
+    mainPanel.add(submitButton);
+    mainPanel.add(newButton);
+
+    //add the text field to the panel
+    mainPanel.add(input);
+
+    //add the labels to the main panel 
+    mainPanel.add(instructions);
+    mainPanel.add(result);
+  
+    //add the panel to the window
+    frame.add(mainPanel);
     
 
   }
@@ -27,6 +91,30 @@ public class Main implements Runnable, ActionListener{
   public void actionPerformed(ActionEvent e){
     // get the command from the action
     String command = e.getActionCommand();
+
+
+    if(command.equals("submit")){
+
+      //get text from input and change it into an integer
+      String inputText = input.getText();
+      int inputInt = Integer.parseInt(inputText);
+
+      //check to see how the guess compares to the random integer
+      if(inputInt == randInt){
+        result.setText("You are correct! Good job!");
+      }else if (inputInt < randInt){
+        result.setText("Your guess of " + inputInt + " is too low!");
+      }else if (inputInt > randInt){
+        result.setText("Your guess of " + inputInt + " is too high!");
+      }
+    }else if(command.equals("new")){
+
+      //clear the text in the text box
+      input.setText("");
+
+      //get the random number generator to get a number
+      randInt = rand.nextInt(100) + 1;
+    }
 
   }
 
